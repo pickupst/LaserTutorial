e9889297df1217433345aaeb9822841a4159a71f
pickupst <gd.okanuslu@gmail.com>
2019-01-20 21:16:17 +0300

    Add basic laser to forward

---
+++ Assets/Scripts/scriptLaser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scriptLaser : MonoBehaviour
+{
+    LineRenderer line;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        line = gameObject.GetComponent<LineRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray ray = new Ray(transform.position, transform.forward);
+        line.SetPosition(0, ray.origin);
+        line.SetPosition(1, ray.GetPoint(100));
+
+    }
+}
