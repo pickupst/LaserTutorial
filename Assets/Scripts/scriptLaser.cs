ab7d1ebbb20b126adbf8df4435e0026fd83019ea
pickupst <gd.okanuslu@gmail.com>
2019-01-20 21:27:28 +0300

    Add force for target

--- Assets/Scripts/scriptLaser.cs
+++ Assets/Scripts/scriptLaser.cs
@@ -11,6 +11,7 @@ public class scriptLaser : MonoBehaviour
     {
 
         line = gameObject.GetComponent<LineRenderer>();
+        line.enabled = false;
 
     }
 
@@ -18,9 +19,42 @@ public class scriptLaser : MonoBehaviour
     void Update()
     {
 
-        Ray ray = new Ray(transform.position, transform.forward);
-        line.SetPosition(0, ray.origin);
-        line.SetPosition(1, ray.GetPoint(100));
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StopCoroutine("FireLaser");
+            StartCoroutine("FireLaser");
+        }
 
+    }
+
+    IEnumerator FireLaser()
+    {
+        line.enabled = true;
+
+        while (Input.GetButton("Fire1")) { 
+            Ray ray = new Ray(transform.position, transform.forward);
+
+            RaycastHit hit;
+
+            line.SetPosition(0, ray.origin);
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                line.SetPosition(1, hit.point);
+
+                if (hit.rigidbody)
+                {
+                    hit.rigidbody.AddForceAtPosition(transform.forward * 100, hit.point);
+                }
+            }
+            else
+            {
+                line.SetPosition(1, ray.GetPoint(100));
+            }
+
+            
+            yield return null;
+        }
+        line.enabled = false;
     }
 }
